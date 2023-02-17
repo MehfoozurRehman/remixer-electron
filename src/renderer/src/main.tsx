8458c435ee94571c9afc694e4fe061e33e32541b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-02-18 03:22:57 +0500

    done

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,4 +1,4 @@
-import App from "./App";
+import { Router } from "./router";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+createRoot(document.getElementById("root")).render(<Router />);
